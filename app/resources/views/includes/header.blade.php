2b5e2ada906420135612ce9262435f78830a3c53
Eshanokpe Daniel <eshanokpe@gmail.com>
2023-11-24 02:37:05 +0100

    social

--- app/resources/views/includes/header.blade.php
+++ app/resources/views/includes/header.blade.php
@@ -7,10 +7,6 @@
                         <ul>
                             <li><i class="fi-rs-smartphone"></i> <a href="#">{{$settings->site_phone}}</a></li>
                             <li><i class="fi-rs-marker"></i><a  href="page-contact.html">{{$settings->site_email}}</a></li>
-                            <li><i class="fi-rs-smartphone"></i> 
-                                <a href="#">{{$settings->site_phone}} </a>
-                                </li>
-                            <li><i class="fi-rs-envelope"></i><a href="#">{{$settings->site_email}} </a></li>
                         </ul>
                     </div>
                 </div>
@@ -52,28 +48,6 @@
                                     @csrf
                                 </form> 
                             @endguest
-                            <li>
-                                <a class="language-dropdown-active" href="#"> <i class="fi-rs-world"></i> English <i class="fi-rs-angle-small-down"></i></a>
-                                <ul class="language-dropdown">
-                                
-                                </ul>
-                            </li>
-                            @guest
-                            <li><i class="fi-rs-user"></i><a href="{{route('login')}}">Log In / Sign Up</a></li>
-                        @else
-                            <li>
-                                <a class="language-dropdown-active" href="{{route('users.account')}}"> <i class="fi-rs-world"></i> Account <i class="fi-rs-angle-small-down"></i></a>
-                                <ul class="language-dropdown">
-                                    <li><a href="{{route('users.orders')}}" >Orders</a></li>
-                                    <li><a href="{{route('user.account.details')}}" >Settings</a></li>
-                                    <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</a></li>
-                                    
-                                </ul>
-                            </li> 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form> 
-                        @endguest
                         </ul>
                     </div>
                 </div>
@@ -101,19 +75,7 @@
                     </div>
                     
                     
-                <div class="logo logo-width-1" >
-                   <a href="{{route('index')}}"><img style="width:200px"  src="{{ asset('/assets/logo.jpeg')}}" alt="logo"></a>
-                </div>
-                <div class="header-right">
-                    <div class="search-style-2">
-                        {{Form::open(['action' => 'HomeController@search', 'method'=>'get', 'class'=>'js-focus-state'])}}
-                           
-                            <select class="select-active">
-                                <option>All Categories</option>
-                            </select>
-                            <input type="text" placeholder="Search for items...">
-                            {{ Form::close()}}
-                    </div>
+
                     <div class="header-action-right">
                         <div class="header-action-2">
                             <div class="header-action-icon-2">
@@ -127,11 +89,6 @@
                                         @endif
                                     </span>  
                                    
-                                    @if(Cart::count()> 0) 
-                                        <span class="pro-count blue ">{{Cart::count()}}</span>  
-                                    @else  
-                                        <span class="pro-count blue"> 0 </span>
-                                    @endif
                                 </a>
                                 <div class="cart-dropdown-wrap cart-dropdown-hm2">
                                     <ul>
@@ -157,7 +114,6 @@
                                         @endif
                                     </ul>
                                     @php
-
                                         $priceTotal = Cart::priceTotal();
                                         $tax = $priceTotal * 0.12; // Calculate the tax amount (12% of the subtotal)
                                         $totalPrice = $priceTotal + $tax; // Add the tax to the subtotal to get the total price
@@ -173,22 +129,6 @@
                                             <a href="{{route('carts.index')}}" class="outline">View cart</a>
                                             <a href="{{route('checkout.index')}}">Checkout</a>
                                         </div>
-                                @php
-                                    $priceTotal = Cart::priceTotal();
-                                    $tax = $priceTotal * 0.12; // Calculate the tax amount (12% of the subtotal)
-                                    $totalPrice = $priceTotal + $tax; // Add the tax to the subtotal to get the total price
-                                @endphp
-                                <div class="shopping-cart-footer">
-                                    <div class="shopping-cart-total">
-                                        <h4>Subtotal <span>C${{ Cart::subTotal()}}</span></h4>
-                                    </div>
-                                    <div class="shopping-cart-total">
-                                        <h4>Total <span >C${{ number_format($totalPrice, 2)  }}</span></h4>
-                                    </div>
-                                    <div class="shopping-cart-button">
-                                        <a href="{{route('carts.index')}}" class="outline">View cart</a>
-                                        <a href="{{route('checkout.index')}}">Checkout</a>
-                                    </div>
                                     </div>
                                 </div>
                             </div>
@@ -202,61 +142,14 @@
         <div class="container">
             <div class="header-wrap header-space-between position-relative">
                 <div class="logo logo-width-1 d-block d-lg-none">
-                    <a href="{{route('index')}}"><img src="{{ asset('/assets/logo.jpeg')}}" alt="logo"></a>
+                    <a href="index.html"><img src="assets/imgs/theme/logo.svg" alt="logo"></a>
                 </div>
                 <div class="header-nav d-none d-lg-flex">
                    
                     <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block">
                         <nav>
-                            <ul> 
-                                @foreach($menu_categories as $cat )
-                    <a href="index.html"><img src="assets/imgs/theme/logo.svg" alt="logo"></a>
-                </div>
-                <div class="header-nav d-none d-lg-flex">
-                    <div class="main-categori-wrap d-none d-lg-block">
-                        <a class="categori-button-active" href="#">
-                            <span class="fi-rs-apps"></span> Browse Categories
-                        </a>
-                        <div class="categori-dropdown-wrap categori-dropdown-active-large">
                             <ul>
                                 @foreach($menu_categories as $cat )
-                                    
-                                <li class="has-children">
-                                    <a href="{{route('user.category', encrypt($cat->id))}}"><i class="evara-font-dress"></i>{{$cat->name}}</a>
-                                    <div class="dropdown-menu">
-                                        <ul class="mega-menu d-lg-flex">
-                                            <li class="mega-menu-col col-lg-7">
-                                                <ul class="d-lg-flex">
-                                                    <li class="mega-menu-col col-lg-6">
-                                                        <ul>
-                                                            @foreach ($cat->products as $prods )                                                   
-                                                            <li> <a class="dropdown-item nav-link nav_item" href="#"> <i class="evara-font-dress"></i>{{ $prods->name}}</a></li>
-                                                            @endforeach
-                                                        </ul>
-                                                    </li>
-                                                
-                                                </ul>
-                                            </li>
-                                            <li class="mega-menu-col col-lg-5">
-                                                @foreach ($cat->products->take(1) as $prods )   
-                                                <div class="header-banner2">
-                                                    <img src="{{ asset('/images/products/'.$prods->image) }}" alt="menu_banner1">
-                                                </div>
-                                                @endforeach
-
-                                            </li>
-                                         
-                                        </ul>
-                                    </div>
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block">
-                        <nav>
-                            <ul>
-                                @foreach($menu_categories->take(8) as $cat )
                                 <li class="mega-menu-col">
                                     <a href="{{route('user.category', encrypt($cat->id))}}">
                                         {{$cat->name}}
@@ -287,16 +180,6 @@
                             <a class="mini-cart-icon" href="shop-cart.html">
                                 <img alt="Trendy Kay Collection" src="{{ asset('assets/imgs/theme/icons/icon-cart.svg')}}">
                                 <span class="pro-count white cartCount"> 
-                <div class="hotline d-none d-lg-block">
-                    <p><i class="fi-rs-headset"></i><span>Hotline</span> {{$settings->site_phone}} </p>
-                </div>
-                <p class="mobile-promotion">Happy <span class="text-brand">Mother's Day</span>. Big Sale Up to 40%</p>
-                <div class="header-action-right d-block d-lg-none">
-                    <div class="header-action-2">
-                        <div class="header-action-icon-2">
-                            <a class="mini-cart-icon" href="shop-cart.html">
-                                <img alt="Trendy Kay Collection" src="{{ asset('assets/imgs/theme/icons/icon-cart.svg')}}">
-                                <span class="pro-count white">
                                     @if(Cart::count() > 0)
                                         {{Cart::count()}}
                                     @else
@@ -306,10 +189,6 @@
                             </a>
                             <div class="cart-dropdown-wrap cart-dropdown-hm2">
                                 <ul> 
-                                </span>
-                            </a>
-                            <div class="cart-dropdown-wrap cart-dropdown-hm2">
-                                <ul>
                                     @if (Cart::count() > 0) 
                                         @foreach (Cart::content()->take(4) as $carts)
                                     <li>
@@ -335,9 +214,6 @@
                                 $priceTotal = Cart::priceTotal();
                                 $tax = $priceTotal * 0.12; // Calculate the tax amount (12% of the subtotal)
                                 $totalPrice = $priceTotal + $tax; // Add the tax to the subtotal to get the total price
-                                    $priceTotal = Cart::priceTotal();
-                                    $tax = $priceTotal * 0.12; // Calculate the tax amount (12% of the subtotal)
-                                    $totalPrice = $priceTotal + $tax; // Add the tax to the subtotal to get the total price
                                 @endphp
                                 <div class="shopping-cart-footer">
                                     <div class="shopping-cart-total">
@@ -448,9 +324,11 @@
             <div class="mobile-social-icon">
                 <h5 class="mb-15 text-grey-4">Follow Us</h5>
                 <a href="{{ $settings->facebook}}"><img src="assets/imgs/theme/icons/icon-facebook.svg" alt=""></a>
-                <a href="{{ $settings->instagram}}"><img src="assets/imgs/theme/icons/icon-instagram.svg" alt=""></a>
+                <a href="#"><img src="assets/imgs/theme/icons/icon-instagram.svg" alt=""></a>
+                <a href="#"><img src="assets/imgs/theme/icons/icon-pinterest.svg" alt=""></a>
                 <a href="#"><img src="assets/imgs/theme/icons/icon-youtube.svg" alt=""></a>
             </div>
         </div>
     </div>
 </div>
+
