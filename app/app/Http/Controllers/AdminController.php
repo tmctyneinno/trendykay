bd558abd5de1b011eea76f1c9aae0a7c5209a5ff
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-01 01:04:48 +0100

    update

--- app/app/Http/Controllers/AdminController.php
+++ app/app/Http/Controllers/AdminController.php
@@ -200,9 +200,9 @@ class AdminController extends Controller
     }
 
     public function Analytical(){
-        $data['users'] = User::where('updated_at', '>=', Carbon::now()->subHours(12))->latest()->get();
+        $data['users'] = User::where('updated_at', '>=', Carbon::now()->subHours(5000))->latest()->get();
         $data['active'] = count($data['users']);
-        $data['recentActive'] = User::where('updated_at', '>=', Carbon::now()->subMinutes(20))->latest()->get();
+        $data['recentActive'] = User::where('updated_at', '>=', Carbon::now()->subMinutes(10))->latest()->get();
         $data['recent'] = count($data['recentActive']);
         $data['new_users'] = User::where('created_at', '>=', today()->subHours(12))->latest()->get();
         $data['thisweek'] = User::where('created_at', '>=', today()->subDays(7))->latest()->get();
