eb1412b4ab0bc58181cd7b1d6fccbef8c82f50c1
Michael Ozoudeh <jesmikky@gmail.com>
2023-12-09 11:35:12 +0100

    update

--- app/resources/views/includes/footer.blade.php
+++ app/resources/views/includes/footer.blade.php
@@ -104,7 +104,7 @@
     </div>
 </footer>
 <!-- Preloader Start -->
- <div id="preloader-active">
+ {{-- <div id="preloader-active">
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-inner position-relative">
             <div class="text-center">
@@ -117,7 +117,7 @@
             </div>
         </div>
     </div>
-</div> 
+</div>  --}}
 
 @if(isset($splash))
 @include('includes.flashmsg')
